b2aa417445e28ee9e66f362c237e2ffce5494006
Keshav Saraogi <tuj49866@temple.edu>
2024-07-06 12:52:25 +0530

    convex
    -- retrieving files from convex
    -- simple convex authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,12 @@ import {
   SignedIn, 
   SignedOut 
 } from '@clerk/nextjs';
-import { useMutation } from 'convex/react';
+import { useMutation, useQuery } from 'convex/react';
 import { api } from '../../convex/_generated/api';
 
 export default function Home() {
   const createFile = useMutation(api.files.createFile);
+  const files = useQuery(api.files.getFiles)
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -25,6 +26,13 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      {files?.map(file => {
+        return <div key={file._id}>
+          {file.name}
+        </div>
+      })}
+
       <Button onClick={() => {
         createFile({
           name: "hello world",
