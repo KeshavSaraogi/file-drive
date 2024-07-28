008fbcf3379cc68232a0e095fc7bf8800c9e7d98
Keshav Saraogi <tuj49866@temple.edu>
2024-07-28 17:43:39 +0530

    temporary fix

--- convex/schema.ts
+++ convex/schema.ts
@@ -27,7 +27,7 @@ export default defineSchema({
   }).index("by_userId_orgId_fileId", ["userId", "orgId", "fileId"]),
   
   users: defineTable({
-    tokenIdentifier: v.string(),
+    tokenIdentifier: v.optional(v.string()),
     name: v.optional(v.string()),
     image: v.optional(v.string()),
      orgIds: v.array(
